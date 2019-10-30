c16b11b5b5de6269007282951dae450cc8ec9791
younghwakim <younghwa.kym@gmail.com>
2019-10-30 20:43:58 +0900

    #11.5 Message Navigation part Two

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,19 +1,48 @@
+import React from "react";
 import { View } from "react-native";
 import { createBottomTabNavigator } from "react-navigation-tabs";
-import Home from "../screens/Home";
-import Search from "../screens/Search";
-import Notifications from "../screens/Notifications";
-import Profile from "../screens/Profile";
+import { createStackNavigator } from "react-navigation-stack";
+
+import Home from "../screens/Tabs/Home";
+import Search from "../screens/Tabs/Search";
+import Notifications from "../screens/Tabs/Notifications";
+import Profile from "../screens/Tabs/Profile";
+import MessagesLink from "../components/MessagesLink";
+
+const stackFactory = (initialRoute, customConfig) =>
+  createStackNavigator({
+    InitialRoute: {
+      screen: initialRoute,
+      navigationOptions: { ...customConfig }
+    }
+  });
 
 export default createBottomTabNavigator({
-    Home,
-    Search,
-    Add: {
-        screen: View,
-        navigationOptions: {
-            tabBarOnPress: ({ navigation }) => navigation.navigate("PhotoNavigation")
-        }
-    },
-    Notifications,
-    Profile
+  Home: {
+    screen: stackFactory(Home, {
+      title: "Home",
+      headerRight: <MessagesLink />
+    })
+  },
+  Search: {
+    screen: stackFactory(Search, {
+      title: "Search"
+    })
+  },
+  Add: {
+    screen: View,
+    navigationOptions: {
+      tabBarOnPress: ({ navigation }) => navigation.navigate("PhotoNavigation")
+    }
+  },
+  Notifications: {
+    screen: stackFactory(Notifications, {
+      title: "Notifications"
+    })
+  },
+  Profile: {
+    screen: stackFactory(Profile, {
+      title: "Profile"
+    })
+  }
 });
