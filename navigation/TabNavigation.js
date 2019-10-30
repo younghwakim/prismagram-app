efb2b6817bab977a9748dde9c20fcccac9fe0314
younghwakim <younghwa.kym@gmail.com>
2019-10-30 19:43:24 +0900

    #11.3 Photo Navigation

---
+++ navigation/TabNavigation.js
@@ -0,0 +1,19 @@
+import { View } from "react-native";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import Home from "../screens/Home";
+import Search from "../screens/Search";
+import Notifications from "../screens/Notifications";
+import Profile from "../screens/Profile";
+
+export default createBottomTabNavigator({
+    Home,
+    Search,
+    Add: {
+        screen: View,
+        navigationOptions: {
+            tabBarOnPress: ({ navigation }) => navigation.navigate("PhotoNavigation")
+        }
+    },
+    Notifications,
+    Profile
+});
