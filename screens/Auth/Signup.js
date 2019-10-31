d26a05cc34b84a57e90caef90fbb67122cffd455
younghwakim <younghwa.kym@gmail.com>
2019-10-31 19:32:38 +0900

    #12.9 Facebook Login part Two

--- screens/Auth/Signup.js
+++ screens/Auth/Signup.js
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { Alert, TouchableWithoutFeedback, Keyboard } from "react-native";
+import { LOG_IN, CREATE_ACCOUNT } from "./AuthQueries";
+import { useMutation } from "react-apollo-hooks";
+import * as Facebook from 'expo-facebook';
 import AuthButton from "../../components/AuthButton";
 import AuthInput from "../../components/AuthInput";
 import useInput from "../../hooks/useInput";
-import { useMutation } from "react-apollo-hooks";
-import { LOG_IN, CREATE_ACCOUNT } from "./AuthQueries";
 
 const View = styled.View`
     justify-content: center;
     align-items: center;
     flex: 1;
+`;
+
+const FBContainer = styled.View`
+  margin-top: 25px;
+  padding-top: 25px;
+  border-top-width: 1px;
+  border-top-color: ${props => props.theme.lightGrayColor};
+  border-style: solid;
 `;
 
 export default ({ navigation }) => {
@@ -63,34 +72,69 @@ export default ({ navigation }) => {
       setLoading(false);
     }
   };
+  const fbLogin = async() => {
+    try {
+      setLoading(true);
+      const { type, token } = await Facebook.logInWithReadPermissionsAsync(
+        "555826795164164",
+        {
+          permissions: ["public_profile", "email"]
+        }
+      );
+      if (type === 'success') {
+        const response = await fetch(`https://graph.facebook.com/me?access_token=${token}&fields=id,last_name,first_name,email`);
+        const { email, first_name, last_name } = await response.json();
+        emailInput.setValue(email);
+        fNameInput.setValue(first_name);
+        lNameInput.setValue(last_name);
+        setLoading(false);
+        const [username] = email.split("@");
+        userNameInput.setValue(username);
+      } else {
+        // type === 'cancel'
+      }
+    } catch ({ message }) {
+      alert(`Facebook Login Error: ${message}`);
+    }
+  };
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View>
-        <AuthInput
-          {...fNameInput}
-          placeholder="Firts name"
-          autoCapitalize="words"
-        />
-        <AuthInput
-          {...lNameInput}
-          placeholder="Last name"
-          autoCapitalize="words"
-        />
-        <AuthInput
-          {...emailInput}
-          placeholder="Email"
-          keyboardType="email-address"
-          returnKeyType="send"
-          autoCorrect={false}
-        />
-        <AuthInput
-          {...userNameInput}
-          placeholder="Username"
-          returnKeyType="send"
-          autoCorrect={false}
-        />
-        <AuthButton loading={loading} text="Sign Up" onPress={handleSignup} />
-      </View>
-    </TouchableWithoutFeedback>
+    <>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View>
+          <AuthInput
+            {...fNameInput}
+            placeholder="Firts name"
+            autoCapitalize="words"
+          />
+          <AuthInput
+            {...lNameInput}
+            placeholder="Last name"
+            autoCapitalize="words"
+          />
+          <AuthInput
+            {...emailInput}
+            placeholder="Email"
+            keyboardType="email-address"
+            returnKeyType="send"
+            autoCorrect={false}
+          />
+          <AuthInput
+            {...userNameInput}
+            placeholder="Username"
+            returnKeyType="send"
+            autoCorrect={false}
+          />
+          <AuthButton loading={loading} text="Sign Up" onPress={handleSignup} />
+          <FBContainer>
+            <AuthButton
+              bgColor={"#2D4DA7"}
+              loading={false}
+              onPress={fbLogin}
+              text="Facebook으로 로그인"
+            />
+          </FBContainer>
+        </View>
+      </TouchableWithoutFeedback>
+    </>
   );
 };
