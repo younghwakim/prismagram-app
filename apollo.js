eaff9c158dc4434c7ef1de2189e91904009329ad
younghwakim <younghwa.kym@gmail.com>
2019-11-12 22:29:11 +0900

    no comment

--- apollo.js
+++ apollo.js
@@ -1,7 +1,5 @@
 const options = {
-    // uri: "http://192.168.43.140:4000"
-    uri: "http://192.168.0.8:4000"
-    // uri: "http://192.168.0.5:4000"
+    uri: "http://127.0.0.1:4000"
 };
 
 export default options;
