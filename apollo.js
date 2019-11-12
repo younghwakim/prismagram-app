a5d46820aa05291f6f62c823e84619e1b3f6210d
younghwakim <younghwa.kym@gmail.com>
2019-11-12 22:27:55 +0900

    #18.6 Navigation to Upload Photo

--- apollo.js
+++ apollo.js
@@ -1,6 +1,7 @@
 const options = {
     // uri: "http://192.168.43.140:4000"
     uri: "http://192.168.0.8:4000"
+    // uri: "http://192.168.0.5:4000"
 };
 
 export default options;
--- navigation/PhotoNavigation.js
+++ navigation/PhotoNavigation.js
@@ -4,19 +4,55 @@ import SelectPhoto from "../screens/Photo/SelectPhoto";
 import TakePhoto from "../screens/Photo/TakePhoto";
 import UploadPhoto from "../screens/Photo/UploadPhoto";
 import { stackStyles } from "./config";
+import styles from "../styles";
 
 const PhotoTabs = createMaterialTopTabNavigator({
-    SelectPhoto,
-    TakePhoto
+    Take: {
+        screen: TakePhoto,
+        navigationOptions: {
+            tabBarLabel: "Take"
+        }
+    },
+    Select: {
+        screen: SelectPhoto,
+        navigationOptions: {
+            tabBarLabel: "Select"
+        }
+    }
 }, {
-    tabBarPosition: "bottom"
+    tabBarPosition: "bottom",
+    tabBarOptions: {
+        indicatorStyle: {
+            backgroundColor: styles.blackColor
+        },
+        labelStyle: {
+            color: styles.blackColor,
+            fontWeight: "600"
+        },
+        style: {
+            ...stackStyles
+        },
+    }
 });
 
 export default createStackNavigator({
-    PhotoTabs,
-    UploadPhoto
+    Tabs: {
+        screen: PhotoTabs,
+        navigationOptions: {
+            title: "Choose Photo",
+            headerBackTitle: null
+        }
+    },
+    Upload: {
+        screen: UploadPhoto,
+        navigationOptions: {
+            title: "Upload"
+        }
+    }
 }, {
+    headerLayoutPreset: "center",
     defaultNavigationOptions: {
-        headerStyle: { ...stackStyles }
+        headerStyle: { ...stackStyles },
+        headerTintColor: styles.blackColor
     }
 })
